a4a93605dc9aca1cd47eba960cdd7c752e20343d
hongbo.wang <hongbo.wang@nxp.com>
2021-01-05 14:13:22 +0800

    change linux-demo
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

--- configs/arm64/ls1043a-rdb-linux-demo.c
+++ configs/arm64/ls1043a-rdb-linux-demo.c
@@ -16,7 +16,7 @@
 struct {
 	struct jailhouse_cell_desc cell;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[16];
+	struct jailhouse_memory mem_regions[24];
 	struct jailhouse_irqchip irqchips[2];
 	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
