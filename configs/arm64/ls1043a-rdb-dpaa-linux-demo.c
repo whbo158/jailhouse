1d27af322e8a3a7579d9c8bf610556405a63153b
hongbo.wang <hongbo.wang@nxp.com>
2021-03-02 11:51:34 +0800

    configs: ls1043ardb: Add gpio1 in non-root config and dts file
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

--- configs/arm64/ls1043a-rdb-dpaa-linux-demo.c
+++ configs/arm64/ls1043a-rdb-dpaa-linux-demo.c
@@ -1,7 +1,7 @@
 /*
  * ls1043a RDB target - linux-demo
  *
- * Copyright 2020 NXP
+ * Copyright 2020-2021 NXP
  *
  * Authors:
  *  Hongbo Wang <hongbo.wang@nxp.com>
@@ -16,7 +16,7 @@
 struct {
 	struct jailhouse_cell_desc cell;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[23];
+	struct jailhouse_memory mem_regions[24];
 	struct jailhouse_irqchip irqchips[2];
 	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
@@ -133,6 +133,13 @@ struct {
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},
+		/* gpio1 */ {
+			.phys_start = 0x02310000,
+			.virt_start = 0x02310000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
 		/* dcfg */ {
 			.phys_start = 0x01ee0000,
 			.virt_start = 0x01ee0000,
@@ -171,7 +178,7 @@ struct {
 				1 << (60 - 32)  | 1 << (61 - 32) |
 					1 << (62 - 32) | 1 << (63 -32), /* vPCI */
 				1 << (44 + 32 - 64) | 1 << (45 + 32 - 64),
-				0,
+				1 << (67 + 32 - 96),
 				0,
 			},
 		},
