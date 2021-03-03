a8a67a352c834112184f95a798ef0e5d1428cc4e
hongbo.wang <hongbo.wang@nxp.com>
2021-03-03 16:02:45 +0800

    dl.sh
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

--- dl.sh
+++ dl.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-scp
-whb@10.193.20.39:/mnt/vda3/software/storage/jailhouse/siemens_jailhouse/bin_arm.tgz
-./
+date -s "2022-01-01 00:00:00"
+scp whb@10.193.20.39:/mnt/vda3/software/storage/jailhouse/siemens_jailhouse/bin_arm.tgz ./
 tar xvzf bin_arm.tgz
