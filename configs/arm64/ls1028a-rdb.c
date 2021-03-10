4be87054d28ff02f727e9651dfe08e96e9de0964
hongbo.wang <hongbo.wang@nxp.com>
2021-03-10 11:51:52 +0800

    fix enable issue
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

--- configs/arm64/ls1028a-rdb.c
+++ configs/arm64/ls1028a-rdb.c
@@ -16,7 +16,7 @@
 struct {
 	struct jailhouse_system header;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[73];
+	struct jailhouse_memory mem_regions[74];
 	struct jailhouse_irqchip irqchips[3];
 	struct jailhouse_pci_device pci_devices[2];
 } __attribute__((packed)) config = {
@@ -399,6 +399,13 @@ struct {
 		/* CCI-400 */ {
 			.phys_start = 0x04090000,
 			.virt_start = 0x04090000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* GITS */ {
+			.phys_start = 0x06020000,
+			.virt_start = 0x06020000,
 			.size = 0x10000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
