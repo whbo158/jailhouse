26ac7ffcd27aab609fc4a8d41b4be1664344a0e2
hongbo.wang <hongbo.wang@nxp.com>
2021-03-15 14:02:18 +0800

    add script for ls1028ardb
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

---
+++ bin_arm_base/scripts/gic-demo-ls1028ardb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ..
+
+insmod jailhouse.ko;
+
+./jailhouse disable
+
+./jailhouse enable ls1028a-rdb.cell;
+./jailhouse cell create ls1028a-rdb-inmate-demo.cell;
+./jailhouse cell load 1 ./gic-demo.bin
+./jailhouse cell start 1
+
